17374da2a0e3150ade2eb3fdca9e765d94913e92
Tharun0901 <moodtharu9@gmail.com>
2025-07-08 17:43:58 +0530

    api test cases

--- test_api.py
+++ test_api.py
@@ -1,29 +1,32 @@
 from fastapi.testclient import TestClient
 from main import app
+import random
 test=TestClient(app)
+
+account_no=str(random.randint(100,9999999999))
 def test_create_account():
     res=test.post("/create_account",json={
         "name":"tharun",
         "age":22,
         "branch":"hyd",
-        "account_no":"123123",
+        "account_no":account_no,
         "balance":1000
     })
     assert res.json()["message"]=="Account created successfully"
 
 def test_get_account():
-    res=test.get("/account/123123")
-    assert res.json()["account_no"] == "123123"
+    res=test.get(f"/account/{account_no}")
+    assert res.json()["account_no"] == account_no
 def test_deposit():
     res=test.post("/deposit",json={
-        "account_no":"123123",
+        "account_no":account_no,
         "amount":500
 
     })
     assert res.json()["new_balance"]==1500
 def test_withdraw():
     res=test.post("/withdraw",json={
-        "account_no":"123123",
+        "account_no":account_no,
         "amount":300
     })
     assert res.json()["new_balance"]==1200
