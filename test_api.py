7a72d30f5cfcba0f89d326946529c04f02446fdd
Tharun0901 <moodtharu9@gmail.com>
2025-07-09 12:34:47 +0530

    addded mocking

--- test_api.py
+++ test_api.py
@@ -1,10 +1,17 @@
 from fastapi.testclient import TestClient
 from main import app
+from unittest.mock import patch,MagicMock
 import random
 test=TestClient(app)
 
 account_no=str(random.randint(100,9999999999))
-def test_create_account():
+@patch("bank.cx_Oracle.connect")
+def test_create_account(mock_connect):
+    mock_conn=MagicMock()
+    mock_cursor=MagicMock()
+    mock_cursor.fetchone.side_effect=[None]
+    mock_conn.return_value=mock_cursor
+    mock_connect.return_value=mock_conn
     res=test.post("/create_account",json={
         "name":"tharun",
         "age":22,
@@ -13,20 +20,38 @@ def test_create_account():
         "balance":1000
     })
     assert res.json()["message"]=="Account created successfully"
-
-def test_get_account():
+@patch("bank.cx_Oracle.connect")
+def test_get_account(mock_connect):
+    mock_conn=MagicMock()
+    mock_cursor=MagicMock()
+    mock_cursor.fetchone.return_value=("tharun",22,"hyd",account_no,1000.0)
+    mock_conn.cursor.return_value=mock_cursor
+    mock_connect.return_value=mock_conn
     res=test.get(f"/account/{account_no}")
     assert res.json()["account_no"] == account_no
-def test_deposit():
-    res=test.post("/deposit",json={
+    assert res.json()["balance"] == 1000.0
+@patch("bank.cx_Oracle.connect")
+def test_deposit(mock_connect):
+     mock_conn = MagicMock()
+     mock_cursor = MagicMock()
+     mock_cursor.fetchone.return_value=("tharun",22,"hyd",account_no,1000.0)
+     mock_conn.cursor.return_value=mock_cursor
+     mock_connect.return_value=mock_conn
+     res=test.post("/deposit",json={
         "account_no":account_no,
         "amount":500
 
     })
-    assert res.json()["new_balance"]==1500
-def test_withdraw():
+     assert res.json()["new_balance"]==1500
+@patch("bank.cx_Oracle.connect")
+def test_withdraw(mock_connect):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_cursor.fetchone.return_value=("tharun",22,"hyd",account_no,1000.0)
+    mock_conn.cursor.return_value=mock_cursor
+    mock_connect.return_value=mock_conn
     res=test.post("/withdraw",json={
         "account_no":account_no,
         "amount":300
     })
-    assert res.json()["new_balance"]==1200
+    assert res.json()["new_balance"]==700
